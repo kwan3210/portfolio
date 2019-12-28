40084e1d8ea31e656cb471be083ae628e53774b9
kwan3210 <41954102+kwan3210@users.noreply.github.com>
2019-12-28 15:21:15 +1000

    Add files via upload
    js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/*Scroll Back to Top tutorial from https://www.w3schools.com/howto/howto_js_scroll_to_top.asp*/
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
